cadbf1c37a96f7781f47e0c6402bd42ad5b05855
Daniel Rieck, Ph.D <danielcrieck@gmail.com>
2020-01-29 23:40:01 -0800

    Update name in license

--- gulpfile.js
+++ gulpfile.js
@@ -16,10 +16,10 @@
  *   without fee is hereby granted, provided that the above copyright notice and this permission
  *   notice appear in all copies.
  *
- *   THE SOFTWARE IS PROVIDED "AS IS" AND DANIEL RIECK DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS
- *   SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
- *   DANIEL RIECK BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
- *   DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+ *   THE SOFTWARE IS PROVIDED "AS IS" AND DANIEL C. RIECK DISCLAIMS ALL WARRANTIES WITH REGARD TO
+ *   THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT
+ *   SHALL DANIEL C. RIECK BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR
+ *   ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
  *   CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
  *   PERFORMANCE OF THIS SOFTWARE.
  **************************************************************************************************/
@@ -30,8 +30,8 @@
 // §1: Gulp task dependencies..................................................................43
 // §2: Specificiation of build settings .......................................................48
 //   §2.1: getCssBuildSettings()...............................................................51
-//   §2.2: getJsBuildSettings()...............................................................100
-// §3: Entry point: Set up of build taks......................................................126
+//   §2.2: getJsBuildSettings()...............................................................105
+// §3: Entry point: Set up of build taks......................................................131
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -59,32 +59,36 @@ function getCssBuildSettings() {
 	var commentRemovalNeedle = /^(?:[ \t]*)?\/\*[^!].*$\n(?:^\*\*?[^/].*$\n)*\*\*?\/\n\n?/gm;
 	var dependenciesPath = './WSU-UE---CSS/';
 	var destFolder = './CSS/';
-	var fontImportStr = '@import url(\'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,4\
-00,400i,700,700i|Roboto+Condensed:400,400i,700,700i|PT+Serif:400,400i,700,700i|Roboto+Mono:400,400i\
-,700,700i&display=swap\');\r\n';
+	var fontImportStr = '@import url(\'https://fonts.googleapis.com/css?family=Open+Sans:300,300i' +
+		'00,400i,700,700i|Roboto+Condensed:400,400i,700,700i|PT+Serif:400,400i,700,700i|Roboto+Mo' +
+		'no:400,400i,700,700i&display=swap\');\r\n';
 	var insertingMediaQuerySectionHeader = {
 			'before': /^@media/,
-			'lineBefore': '/*! ====================================================================\
-============================\r\n*** Media queries section\r\n*** ==================================\
-==============================================================\r\n***   SUMMARY: Media queries buil\
-t from precompiled CSS written in the Less language extension of\r\n***    CSS. Queries in this sec\
-tion are a combination of those designed for use on DAESA websites\r\n***    and those intended spe\
-cifically for use on the Academic Success and Career Center website.\r\n***\r\n***   DESCRIPTION: F\
-ully documented, precompiled source code from which this section of the custom\r\n***    stylesheet\
- was built is developed and maintained on the following two GitHub projects:\r\n***    https://gith\
-ub.com/invokeImmediately/WSU-UE---CSS/\r\n***    https://github.com/invokeImmediately/ascc.wsu.edu/\
-\r\n***\r\n***   AUTHOR: Daniel Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)\
-\r\n***\r\n***   LICENSE: ISC - Copyright (c) 2020 Daniel C. Rieck.\r\n***\r\n***     Permission to\
- use, copy, modify, and/or distribute this software for any purpose with or\r\n***     without fee \
-is hereby granted, provided that the above copyright notice and this permission\r\n***     notice a\
-ppear in all copies.\r\n***\r\n***     THE SOFTWARE IS PROVIDED "AS IS" AND DANIEL RIECK DISCLAIMS \
-ALL WARRANTIES WITH REGARD TO\r\n***     THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANT\
-ABILITY AND FITNESS. IN NO EVENT\r\n***     SHALL DANIEL RIECK BE LIABLE FOR ANY SPECIAL, DIRECT, I\
-NDIRECT, OR CONSEQUENTIAL DAMAGES OR\r\n***     ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, \
-DATA OR PROFITS, WHETHER IN AN ACTION OF\r\n***     CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, \
-ARISING OUT OF OR IN CONNECTION WITH THE USE\r\n***     OR PERFORMANCE OF THIS SOFTWARE.\r\n*** ===\
-=============================================================================================\r\n**\
-/\r\n',
+			'lineBefore': '/*! ==================================================================' +
+				'==============================\r\n*** Media queries section\r\n*** =============' +
+				'================================================================================' +
+				'===\r\n***   SUMMARY: Media queries built from precompiled CSS written in the Le' +
+				'ss language extension of\r\n***    CSS. Queries in this section are a combinatio' +
+				'n of those designed for use on DAESA websites\r\n***    and those intended speci' +
+				'fically for use on the Academic Success and Career Center website.\r\n***\r\n***' +
+				'   DESCRIPTION: Fully documented, precompiled source code from which this sectio' +
+				'n of the custom\r\n***    stylesheet was built is developed and maintained on th' +
+				'e following two GitHub projects:\r\n***    https://github.com/invokeImmediately/' +
+				'WSU-UE---CSS/\r\n***    https://github.com/invokeImmediately/ascc.wsu.edu/\r\n**' +
+				'*\r\n***   AUTHOR: Daniel Rieck [daniel.rieck@wsu.edu] (https://github.com/invok' +
+				'eImmediately)\r\n***\r\n***   LICENSE: ISC - Copyright (c) 2020 Daniel C. Rieck.' +
+				'\r\n***\r\n***     Permission to use, copy, modify, and/or distribute this softw' +
+				'are for any purpose with or\r\n***     without fee is hereby granted, provided t' +
+				'hat the above copyright notice and this permission\r\n***     notice appear in a' +
+				'll copies.\r\n***\r\n***     THE SOFTWARE IS PROVIDED "AS IS" AND DANIEL C. RIEC' +
+				'K DISCLAIMS ALL WARRANTIES WITH REGARD TO\r\n***     THIS SOFTWARE INCLUDING ALL' +
+				' IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT\r\n***     SHALL' +
+				' DANIEL C. RIECK BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL D' +
+				'AMAGES OR\r\n***     ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR ' +
+				'PROFITS, WHETHER IN AN ACTION OF\r\n***     CONTRACT, NEGLIGENCE OR OTHER TORTIO' +
+				'US ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE\r\n***     OR PERFORMANC' +
+				'E OF THIS SOFTWARE.\r\n*** =====================================================' +
+				'===========================================\r\n**/\r\n',
 			'stopAfterFirstMatch': true
 		};
 	var minCssFileExtension = '.min.css';
